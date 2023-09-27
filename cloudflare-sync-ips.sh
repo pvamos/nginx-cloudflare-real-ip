c92ed60fa1fa329ec32595dd399d27884561236f
Péter Vámos <pvamos@gmail.com>
2023-09-27 13:42:12 +0000

    port to FreeBSD tcsh (from bash)

--- cloudflare-sync-ips.sh
+++ cloudflare-sync-ips.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/bin/tcsh
 
-CLOUDFLARE_FILE_PATH=${1:-/etc/nginx/cloudflare}
+set CLOUDFLARE_FILE_PATH="/usr/local/etc/nginx/cloudflare"
 
 echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH;
 echo "" >> $CLOUDFLARE_FILE_PATH;
 
 echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
-for i in `curl -s -L https://www.cloudflare.com/ips-v4`; do
-        echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
-done
+foreach i (`curl -s -L https://www.cloudflare.com/ips-v4`)
+    echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+end
 
 echo "" >> $CLOUDFLARE_FILE_PATH;
 echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
-for i in `curl -s -L https://www.cloudflare.com/ips-v6`; do
-        echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
-done
+foreach i (`curl -s -L https://www.cloudflare.com/ips-v6`)
+    echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
+end
 
 echo "" >> $CLOUDFLARE_FILE_PATH;
 echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH;
